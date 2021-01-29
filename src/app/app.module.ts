7eac431eed1dc253f72800c3919f62f0992a3214
keithlin <keithlin@awt-system.com.tw>
2021-01-29 10:04:12 +0800

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {TuiDialogModule, TuiNotificationsModule, TuiRootModule} from '@taiga-ui/core';
+import {iconsPathFactory, TUI_ICONS_PATH} from '@taiga-ui/core';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    TuiRootModule,
+    TuiNotificationsModule,
+    TuiDialogModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [ {
+    provide: TUI_ICONS_PATH,
+    useValue: iconsPathFactory('assets/taiga-ui/icons/'),
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
